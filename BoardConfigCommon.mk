e75c2178df1437e6ccbdf86034e924959b5e46f0
MD Raza <delightreza@gmail.com>
2024-04-22 17:22:41 +0530

    sm6225-common: drop common matrix and framework manifest
    Signed-off-by: MD Raza <delightreza@gmail.com>
    Change-Id: I311f12dac55603d82ac55c517e5f5328ff48e297

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -66,10 +66,8 @@ LOC_HIDL_VERSION := 4.0
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(COMMON_PATH)/framework_compatibility_matrix.xml \
-    vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
+    $(COMMON_PATH)/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
-DEVICE_MATRIX_FILE += device/qcom/vendor-common/compatibility_matrix.xml
 ODM_MANIFEST_FILES += $(COMMON_PATH)/manifest-qva.xml
 
 # Init
